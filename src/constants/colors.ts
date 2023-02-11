76a48b3d2b67da46acbe9bcf2627644a9409faea
HubOneX <hubciox@gmail.com>
2023-02-11 21:03:55 +0100

    TDV-3: Added new lines at the ends of the files

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,7 +1,7 @@
 const colors = {
-    white: "#FFFFFF",
-    lightGrey: "#FAFAFA",
-    midGrey: "#42445A"
-}
+  white: "#FFFFFF",
+  lightGrey: "#FAFAFA",
+  midGrey: "#42445A",
+};
 
 export default colors;
--- src/themes/lightTheme.ts
+++ src/themes/lightTheme.ts
@@ -5,7 +5,7 @@ const lightTheme = createTheme();
 lightTheme.typography.h3 = {
   fontWeight: 500,
   fontSize: "30px",
-  marginBottom: '8px',
+  marginBottom: "8px",
   [lightTheme.breakpoints.down("sm")]: {
     fontSize: "20px",
   },
@@ -14,7 +14,7 @@ lightTheme.typography.h3 = {
 lightTheme.typography.subtitle1 = {
   fontWeight: 400,
   fontSize: "20px",
-  marginBottom: '6px',
+  marginBottom: "6px",
   [lightTheme.breakpoints.down("sm")]: {
     fontSize: "10px",
   },
