14f84ba8a1fc7d62cbcf17d41fde688e9d97eaae
HubOneX <hubciox@gmail.com>
2023-02-19 23:55:14 +0100

    IMP-11: Fixed errors in App tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,36 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
+import useFetchHotels from "./hooks/useFetchHotels/useFetchHotels";
+import { hotelsMockResponse } from "./mocks/hotelsMockResponse";
+import { roomsMockResponse } from "./mocks/roomsMockResponse";
 
+jest.mock("./hooks/useFetchHotels/useFetchHotels.tsx", () => jest.fn());
 describe("App", () => {
-  it("should render Hero", () => {
+  beforeEach(() => {
+    //@ts-ignore
+    useFetchHotels.mockImplementation(() => ({
+      fetchHotels: () => {
+        return [{ ...hotelsMockResponse[0], roomsData: roomsMockResponse }];
+      },
+    }));
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should render Hero", async () => {
     render(<App />);
-    const foundElement = screen.getByText("HotelsFindr");
+    const foundElement = await screen.findByText("HotelsFindr");
     expect(foundElement).toBeInTheDocument();
   });
 
-  it("should not render hotels view when without data", () => {
+  it("should not render hotels view when without data", async () => {
     render(<App />);
-    const foundElements = screen.queryByTestId("HotelView");
+    let foundElements;
+    await waitFor(() => {
+      foundElements = screen.queryByTestId("HotelView");
+    });
     expect(foundElements).not.toBeInTheDocument();
   });
 
