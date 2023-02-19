9469f7ecf645b40b611e188639f3d364c6519ccc
HubOneX <hubciox@gmail.com>
2023-02-19 19:10:01 +0100

    IMP-9: DataTypes refactor + Updated Loader snapshot

--- src/types/DataTypes.ts
+++ src/types/DataTypes.ts
@@ -1,24 +1,27 @@
-export type HotelData = {
-  id: string;
+export type Id<T = string> = {
+  id: T;
+};
+
+export interface HotelData extends Id {
   hotelName: string;
   hotelAddressFirstLine: string;
   hotelAddressSecondLine: string;
   starRating: number;
   images: ImageWithAlt[];
   roomsData: RoomData[];
-};
-
-export type ImageWithAlt = {
-  url: string,
-  alt: string
 }
 
-export type RoomData = {
+export type ImageWithAlt = {
+  url: string;
+  alt: string;
+};
+
+export interface RoomData extends Id {
   id: string;
   name: string;
   longDescription: string;
   occupancy: Occupancy;
-};
+}
 
 export type Occupancy = {
   maxAdults: number;
